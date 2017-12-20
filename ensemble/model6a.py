2d67f697ba0705113058d1f81a9055bb9c54715b
Roman Bachmann <roman.bachmann@epfl.ch>
2017-12-20 11:45:19 +0100

    Added sentiment and ensemble models

---
+++ ensemble/model6a.py
@@ -0,0 +1,159 @@
+### Final Model 6 ###
+
+import os
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+import gensim
+import Cython
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+from torch.utils.data import Dataset, TensorDataset, DataLoader
+
+from helpers import *
+from data import create_csv_submission
+
+
+torch.manual_seed(61)
+torch.cuda.manual_seed(61)
+# CUDNN has non-deterministic kernels. Uncomment to make deterministic (but slow):
+#torch.backends.cudnn.enabled = False
+
+
+### Step 1: Load the tweets ###
+
+DATA_PATH = '../twitter-datasets/'
+MODEL_PATH = '../models/'
+
+#TRAIN_NEG_PATH = os.path.join(DATA_PATH, 'train_neg.txt') # 100'000 negative tweets
+#TRAIN_POS_PATH = os.path.join(DATA_PATH, 'train_pos.txt') # 100'000 positive tweets
+TRAIN_NEG_PATH = os.path.join(DATA_PATH, 'train_neg_full.txt') # 2'500'000 negative tweets
+TRAIN_POS_PATH = os.path.join(DATA_PATH, 'train_pos_full.txt') # 2'500'000 positive tweets
+TEST_PATH = os.path.join(DATA_PATH, 'test_data.txt')
+
+print("Loading datasets...")
+x_text_train, y_train_full = load_data_and_labels(TRAIN_POS_PATH, TRAIN_NEG_PATH)
+x_text_test = load_test_data(TEST_PATH)
+print("Datasets loaded!")
+
+
+### Step 2: Load word2vec vocabulary ###
+
+vector_length = 100
+print("Loading word2vec model...")
+w2v_model = gensim.models.Word2Vec.load(MODEL_PATH + 'twitter_w2v.bin')
+print("word2vec model loaded!")
+
+# Delete the training model but retain the word vectors:
+word_vectors = w2v_model.wv
+del w2v_model
+
+
+### Step 3: Convert tweets into sentences of vectors ###
+print("Converting tweets into sentences of vectors...")
+
+# Compute the number of words of the longest tweet to get the maximal sentence length
+sequence_length_train = max(len(x) for x in x_text_train)
+sequence_length_test = max(len(x) for x in x_text_test)
+sequence_length = max(sequence_length_train, sequence_length_test)
+print('Maximum sequence length of train and test data:', sequence_length)
+
+x_text_train_pad = pad_sentences(x_text_train, padding_word="<PAD/>", sequence_length=sequence_length)
+x_text_test_pad = pad_sentences(x_text_test, padding_word="<PAD/>", sequence_length=sequence_length)
+
+del x_text_train
+del x_text_test
+
+# Split into training and validation data
+x_train, x_val, y_train, y_val = train_test_split(x_text_train_pad, y_train_full, test_size=0.01, random_state=42)
+
+
+### Step 4: Classification ###
+print("Setting up classification...")
+
+# Hyper Parameters
+num_epochs = 20
+batch_size = 100
+learning_rate = 0.001
+
+train_dataset = ListDataset(x_train, y_train)
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                           batch_size=batch_size,
+                                           shuffle=True)
+
+# Network hyperparameters
+N = len(train_loader.dataset.data_list)     # Number of tweets (eg 200000)
+S = len(train_loader.dataset.data_list[0])  # Number of words in one sentence (eg 50)
+V = vector_length                           # Length of word vectors (eg 100)
+K = 5                                       # Kernel width (K*V)
+C = 128                                     # Number of convolutional filters
+F = 2                                       # Number of output neurons in fully connected layer
+
+cnn = CNN(S, V, K, C, F)
+if torch.cuda.is_available():
+    cnn.cuda()
+print(cnn)
+
+optimizer = torch.optim.Adam(cnn.parameters(), lr=learning_rate)
+if torch.cuda.is_available():
+    loss_func = nn.CrossEntropyLoss().cuda()
+else:
+    loss_func = nn.CrossEntropyLoss()
+
+cnn.train()
+print("Starting training...")
+for epoch in range(num_epochs):  # loop over the dataset multiple times
+    for i, batch_indices in enumerate(train_loader.batch_sampler):   # iterate over mini-batches
+        # Converting tweets to vectors and storing it in a variable
+        sentences = get_tweets_tensor(train_loader.dataset.data_list, word_vectors, vector_length, batch_indices)
+        if torch.cuda.is_available():
+            sentences = sentences.cuda()
+        x = Variable(sentences)
+
+        # Converting labels to a variable
+        labels = torch.from_numpy(train_loader.dataset.target_list[batch_indices])
+        if torch.cuda.is_available():
+            labels = labels.cuda()
+        y = Variable(labels, requires_grad=False)
+
+        # Forward + Backward + Optimize
+        optimizer.zero_grad() # reset gradient
+        outputs = cnn(x) # cnn output
+        loss = loss_func(outputs, y) # clear gradients for this training step
+        loss.backward() # backpropagation, compute gradients
+        optimizer.step() # apply gradients
+
+        if (i+1) % 1000 == 0:
+            print('Epoch [%d/%d], Iter [%d/%d] Loss: %.4f'
+                  %(epoch+1, num_epochs, i+1, len(train_dataset)//batch_size, loss.data[0]))
+
+print('Finished Training')
+
+# Evaluate accuracy of predictions from validation data
+cnn.eval()
+accuracy = 0
+nb_steps = 0
+step_size = 100 # Calculate in steps, since GPU memory might be too small for whole testset
+for i in range(0, len(x_val), step_size):
+    val_output = cnn(Variable(get_tweets_tensor(x_val[i:i+step_size], word_vectors, vector_length).cuda()))
+    y_val_pred = torch.max(val_output.cpu(), 1)[1].data.numpy().squeeze()
+    accuracy += accuracy_score(y_val[i:i+step_size], y_val_pred)
+    nb_steps += 1
+
+print('Validation accuracy:', accuracy/nb_steps)
+
+### Step 5: Make predictions and save model ###
+
+cnn.cpu()
+torch.save(cnn, './ensemble_models/model6.1.pth')
+
+test_output = cnn(Variable(get_tweets_tensor(x_text_test_pad, word_vectors, vector_length)))
+y_pred = torch.max(test_output, 1)[1].data.numpy().squeeze()
+y_pred[y_pred == 0] = -1
+ids = np.arange(len(y_pred)+1)[1:]
+create_csv_submission(ids, y_pred, 'kaggle_model_6.1.csv')
---
+++ sentiment/helpers.py
@@ -0,0 +1,206 @@
+import numpy as np
+import re
+import itertools
+import enchant
+from functools import partial
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
+
+
+
+def clean_str(string):
+    """
+    Tokenization/string cleaning for all datasets except for SST.
+    Original taken from https://github.com/yoonkim/CNN_sentence/blob/master/process_data.py
+    """
+    string = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", string)
+    string = re.sub(r"\'s", " \'s", string)
+    string = re.sub(r"\'ve", " \'ve", string)
+    string = re.sub(r"n\'t", " n\'t", string)
+    string = re.sub(r"\'re", " \'re", string)
+    string = re.sub(r"\'d", " \'d", string)
+    string = re.sub(r"\'ll", " \'ll", string)
+    string = re.sub(r",", " , ", string)
+    string = re.sub(r"!", " ! ", string)
+    string = re.sub(r"\(", " \( ", string)
+    string = re.sub(r"\)", " \) ", string)
+    string = re.sub(r"\?", " \? ", string)
+    string = re.sub(r"\s{2,}", " ", string)
+    return string.strip().lower()
+
+def load_data_and_labels(train_positive_path, train_negative_path):
+    """
+    Loads tweet data from files, splits the data into words and generates labels.
+    Returns split sentences and labels.
+    """
+    # Load data from files
+    positive_examples = list(open(train_positive_path).readlines())
+    positive_examples = [s.strip() for s in positive_examples]
+    negative_examples = list(open(train_negative_path).readlines())
+    negative_examples = [s.strip() for s in negative_examples]
+    # Split by words
+    x_text_train = positive_examples + negative_examples
+    x_text_train = [clean_str(sent) for sent in x_text_train]
+    x_text_train = [s.split(" ") for s in x_text_train]
+    # Generate labels
+    #positive_labels = [[0, 1] for _ in positive_examples]
+    #negative_labels = [[1, 0] for _ in negative_examples]
+    positive_labels = [1 for _ in positive_examples]
+    negative_labels = [0 for _ in negative_examples]
+    y = np.concatenate([positive_labels, negative_labels], 0)
+    return [x_text_train, y]
+
+def load_test_data(test_path):
+    """
+    Loads test data and splits the data into words.
+    Returns split sentences.
+    """
+    # Load data from files
+    x_text_test = list(open(test_path).readlines())
+    x_text_test = [s.strip() for s in x_text_test]
+    # Split by words
+    x_text_test = [clean_str(sent) for sent in x_text_test]
+    x_text_test = [s.split(" ") for s in x_text_test]
+    # Delete the index and comma
+    for x in x_text_test:
+        del x[0]
+        del x[0]
+    return x_text_test
+
+def pad_sentences(sentences, padding_word="<PAD/>", sequence_length=0):
+    """
+    Pads all sentences to the same length. The length is defined by the longest sentence.
+    Returns padded sentences.
+    """
+    if sequence_length <= 0:
+        sequence_length = max(len(x) for x in sentences)
+    padded_sentences = []
+    for i in range(len(sentences)):
+        sentence = sentences[i]
+        num_padding = sequence_length - len(sentence)
+        new_sentence = sentence + [padding_word] * num_padding
+        padded_sentences.append(new_sentence)
+    return padded_sentences
+
+class ListDataset(Dataset):
+    """Dataset wrapping data and target lists.
+
+    Each sample will be retrieved by indexing both lists along the first
+    dimension.
+
+    Arguments:
+        data_list (python list): contains sample data.
+        target_list (python list): contains sample targets (labels).
+    """
+
+    def __init__(self, data_list, target_list):
+        assert len(data_list) == len(target_list)
+        self.data_list = data_list
+        self.target_list = target_list
+
+    def __getitem__(self, index):
+        return self.data_list[index], self.target_list[index]
+
+    def __len__(self):
+        return len(self.data_list)
+
+def get_tweets_tensor(tweets, word_vectors, vector_length, indices=[]):
+    '''Mapping every word to a vector from word2vec
+    Padding words are mapped to zero
+    Leave indices empty to map every tweet in tweets
+    '''
+
+    nb_tweets = len(tweets) if len(indices)==0 else len(indices)
+    tweets_vec = np.zeros((nb_tweets, len(tweets[0]), vector_length), dtype=np.float32)
+
+    if indices == []:
+        for idx_t, tweet in enumerate(tweets):
+            for idx_w, word in enumerate(tweet):
+                if word != '<PAD/>':
+                    tweets_vec[idx_t, idx_w] = word_vectors.wv[word]
+    else:
+        for idx_t, orig_idx in enumerate(indices):
+            for idx_w, word in enumerate(tweets[orig_idx]):
+                if word != '<PAD/>':
+                    tweets_vec[idx_t, idx_w] = word_vectors.wv[word]
+
+    return torch.from_numpy(tweets_vec)
+
+def get_tweets_tensor_tfidf(tweets, word_vectors, vector_length, tfidf, indices=[]):
+    '''Mapping every word to a vector from word2vec
+    Padding words are mapped to zero
+    Leave indices empty to map every tweet in tweets
+    '''
+
+    nb_tweets = len(tweets) if len(indices)==0 else len(indices)
+    tweets_vec = np.zeros((nb_tweets, len(tweets[0]), vector_length), dtype=np.float32)
+
+    if indices == []:
+        for idx_t, tweet in enumerate(tweets):
+            for idx_w, word in enumerate(tweet):
+                if word != '<PAD/>':
+                    tweets_vec[idx_t, idx_w] = word_vectors.wv[word] * tfidf[word]
+    else:
+        for idx_t, orig_idx in enumerate(indices):
+            for idx_w, word in enumerate(tweets[orig_idx]):
+                if word != '<PAD/>':
+                    tweets_vec[idx_t, idx_w] = word_vectors.wv[word] * tfidf[word]
+
+    return torch.from_numpy(tweets_vec)
+
+class CNN(nn.Module):
+    """
+    Two layer convolutional network with batch normaliztation,
+    ReLu activation and dropout after each convolutional layer.
+
+    Keyword arguments:
+    S -- Number of words in one sentence (eg 50)
+    V -- Length of word vectors (eg 300)
+    K -- Kernel width (K*V)
+    C -- Number of convolutional filters
+    F -- Number of output neurons in fully connected layer
+    """
+    def __init__(self, S, V, K, C, F):
+        super(CNN, self).__init__()
+
+        self.bn = nn.BatchNorm1d(C)         # batch normalization
+        self.relu = nn.ReLU()               # ReLU activation
+        self.dropout= nn.Dropout(p=0.2)     # dropout layer
+
+        self.conv1 = nn.Conv2d(             # input shape (1, S, V)
+                in_channels=1,              # input channels
+                out_channels=C,             # number of filters
+                kernel_size=(K,V),          # filter size
+                padding=(K//2,0)            # to keep size S
+        )                                   # output shape (C, S, 1)
+        self.max_pool1 = nn.MaxPool1d(2)    # max-pool each filter into S/2 output
+
+        self.conv2 = nn.Conv1d(             # input shape (C, S/2)
+                in_channels=C,              # input channels
+                out_channels=C,             # number of filters (one for each input channel)
+                kernel_size=K,              # filter size
+                padding=K//2                # to keep size S/2
+        )                                   # output shape (C, S/2)
+        self.max_pool2 = nn.MaxPool1d(S//2) # max pool each filter into 1 output
+
+        self.out = nn.Linear(C, F)          # fully connected layer, output F classes
+        self.softmax = nn.Softmax(dim=1)    # softmax activation function
+
+    def forward(self, x):
+        out = x.unsqueeze(1)
+        out = self.conv1(out).squeeze(3)
+        out = self.bn(out)
+        out = self.relu(out)
+        out = self.dropout(out)
+        out = self.max_pool1(out)
+        out = self.conv2(out)
+        out = self.bn(out)
+        out = self.relu(out)
+        out = self.dropout(out)
+        out = self.max_pool2(out).squeeze(2)
+        out = self.out(out)
+        out = self.softmax(out)
+        return out
